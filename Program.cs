0d2c0ef523d186d8824af0347f2ea97d8011ffad
Kunlapath Paengsa <kunlapath.paengsa@gmail.com>
2023-04-16 20:01:45 +0700

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿public class Solution
+{
+    public double Average00(int[] salary)
+    => salary.Where(w => w != salary.Max() && w != salary.Min()).Average(s => s);
+
+    public double Average01(int[] salary)
+    {
+        double avg = 0;
+        foreach (var element in salary)
+        {
+            avg += element;
+        }
+        return avg / salary.Length;
+    }
+}
