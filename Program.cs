aef9649d79c427dd85f81662ce1b1ede7f8ce2e0
Kunlapath Paengsa <kunlapath.paengsa@gmail.com>
2023-04-16 20:02:46 +0700

    fastest

--- Program.cs
+++ Program.cs
@@ -12,4 +12,30 @@
         }
         return avg / salary.Length;
     }
+
+    public double Average0(int[] salary)
+    {
+
+        double sum = salary[0];
+        double max = salary[0];
+        double min = salary[0];
+
+        for (int i = 1; i < salary.Length; i++)
+        {
+            if (salary[i] > max)
+            {
+                max = salary[i];
+            }
+            else if (salary[i] < min)
+            {
+                min = salary[i];
+            }
+
+            sum += salary[i];
+        }
+
+        double avergae = (sum - max - min) / (salary.Length - 2);
+
+        return avergae;
+    }
 }
